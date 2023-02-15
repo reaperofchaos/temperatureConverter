86e884b67844d4b65db67155e6f898de165ade72
reaperofchaos <tokugawa111@gmail.com>
2023-02-14 22:22:52 -0500

    Initial Commit

---
+++ FahrenheitConverter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FahrenheitConverter
+//
+//  Created by Jacob Conner on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FahrenheitConverter/FahrenheitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FahrenheitConverterApp.swift
+//  FahrenheitConverter
+//
+//  Created by Jacob Conner on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FahrenheitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
